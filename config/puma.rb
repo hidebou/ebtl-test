d1997f02d6ee10c7545155ca87be8e5d091528bd
hidebou <sa.hi.ver2.01@gmail.com>
2018-01-17 18:30:50 +0900

    initial setting

--- config/puma.rb
+++ config/puma.rb
@@ -13,7 +13,7 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+## environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -52,5 +52,10 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # end
 #
 
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
